28843ba29f3d4388e3db944ec743d1bc8ab0e2e6
RIO <raphael.06rio@gmail.com>
2021-11-02 15:35:20 +0100

    add encoder.c/.h

---
+++ Projet_5A/Core/Inc/encoder.h
@@ -0,0 +1,15 @@
+/*
+ * encoder.h
+ *
+ *  Created on: 2 nov. 2021
+ *      Author: rapha
+ */
+
+#ifndef INC_ENCODER_H_
+#define INC_ENCODER_H_
+
+
+
+
+
+#endif /* INC_ENCODER_H_ */
---
+++ Projet_5A/Core/Src/encoder.c
@@ -0,0 +1,10 @@
+/*
+ * encoder.c
+ *
+ *  Created on: 2 nov. 2021
+ *      Author: rapha
+ */
+
+#include "encoder.h"
+
+
