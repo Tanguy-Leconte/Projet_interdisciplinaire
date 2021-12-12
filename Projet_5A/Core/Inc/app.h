9fe2499aafe27a1888ce80c0498bc0b1f629a6ff
RIO <raphael.06rio@gmail.com>
2021-12-12 19:25:27 +0100

    APP

---
+++ Projet_5A/Core/Inc/app.h
@@ -0,0 +1,14 @@
+/*
+ * app.h
+ *
+ *  Created on: Dec 7, 2021
+ *      Author: rapha
+ */
+
+#ifndef INC_APP_H_
+#define INC_APP_H_
+
+void My_app();
+
+
+#endif /* INC_APP_H_ */
---
+++ Projet_5A/Core/Src/app.cpp
@@ -0,0 +1,26 @@
+/*
+ * app.cpp
+ *
+ *  Created on: Dec 7, 2021
+ *      Author: rapha
+ */
+
+#include "Coulomb_meter.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void My_app(){
+	Test_coulomb_meter();
+	while (1)
+	{
+
+	}
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+
