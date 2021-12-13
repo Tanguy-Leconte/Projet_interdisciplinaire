9754886eab342a6b8d14f8d558e05883bb323b9d
RIO <raphael.06rio@gmail.com>
2021-12-13 23:32:31 +0100

    En cours

--- Projet_5A/Core/Inc/display.h
+++ Projet_5A/Core/Inc/display.h
@@ -10,7 +10,7 @@
 
 
 // ###########		INCLUDE		###############
-#include "stm32l4xx_hal_spi.h"
+#include "main.h"
 // ########### 		DEFINE		###############
 
 // ########### 		STRUCTURE	###############
@@ -21,14 +21,17 @@
 class Display{
 	private:
 	// VARS
-
+		uint16_t PIN_CS = GPIO_PIN_6;
+		GPIO_TypeDef * PORT_CS = GPIOA;
+		uint16_t PIN_INIT = GPIO_PIN_9;
+		GPIO_TypeDef * PORT_INIT = GPIOA;
 	// FUNCTIONS
-
+		void write_cmd(uint8_t* cmd)
 	public:
 	// VARS
 
 	//CONSTRUCTORS
-		Display(SPI_HandleTypeDef spi);
+		Display(SPI_HandleTypeDef hspi);
 	// FUNCTIONS
 
 };
--- Projet_5A/Core/Src/display.cpp
+++ Projet_5A/Core/Src/display.cpp
@@ -17,6 +17,21 @@ using namespace std;
 // ########### 		DEFINE		###############
 
 // ########### 		CLASS		###############
-Display::Display(SPI_HandleTypeDef spi){
+Display::Display(SPI_HandleTypeDef hspi){
+	HAL_GPIO_WritePin(PORT_INIT, PIN_INIT, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(PORT_CS, PIN_CS, GPIO_PIN_RESET);
+	write_cmd(mode);
+	HAL_Delay(100);
+	write_cmd(display);
+	HAL_Delay(100);
+	write_cmd(clear);
+	HAL_Delay(100);
+	write_cmd(set);
+	HAL_Delay(100);
+}
 
+// Write a command through SPI
+void Display::write_cmd(uint8_t* cmd){
+	HAL_GPIO_WritePin(PORT_CS, PIN_CS, GPIO_PIN_RESET);
+	HAL_SPI_Transmit(&hspi,cmd,1,1000);
 }
