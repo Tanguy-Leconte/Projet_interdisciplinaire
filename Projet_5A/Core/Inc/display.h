f77e26c04868c8782dfa1f43291e62bc9b0d2255
RIO <raphael.06rio@gmail.com>
2021-12-13 23:44:26 +0100

    Write of display OK - need to test

--- Projet_5A/Core/Inc/display.h
+++ Projet_5A/Core/Inc/display.h
@@ -21,12 +21,17 @@
 class Display{
 	private:
 	// VARS
+		SPI_HandleTypeDef hspi;
+	// CST
 		uint16_t PIN_CS = GPIO_PIN_6;
 		GPIO_TypeDef * PORT_CS = GPIOA;
 		uint16_t PIN_INIT = GPIO_PIN_9;
 		GPIO_TypeDef * PORT_INIT = GPIOA;
 	// FUNCTIONS
-		void write_cmd(uint8_t* cmd)
+		void write_cmd(uint8_t* cmd);
+		void write_data(uint8_t* data);
+		void lcd_print(unsigned char* string);
+		void set_cursor(int column,int line);
 	public:
 	// VARS
 
--- Projet_5A/Core/Src/display.cpp
+++ Projet_5A/Core/Src/display.cpp
@@ -15,9 +15,16 @@
 using namespace std;
 
 // ########### 		DEFINE		###############
+uint8_t display[1]	={0x0C};
+uint8_t clear[1]	={0x01};
+uint8_t set[1]		={0x06};
+uint8_t mode[1]		={0x38};
+uint8_t addr[1]		={0xC5};
+uint8_t data_1[1]	={0x59};
 
 // ########### 		CLASS		###############
-Display::Display(SPI_HandleTypeDef hspi){
+// Initialisation procedure
+Display::Display(SPI_HandleTypeDef hspi):hspi(hspi){
 	HAL_GPIO_WritePin(PORT_INIT, PIN_INIT, GPIO_PIN_RESET);
 	HAL_GPIO_WritePin(PORT_CS, PIN_CS, GPIO_PIN_RESET);
 	write_cmd(mode);
@@ -30,8 +37,31 @@ Display::Display(SPI_HandleTypeDef hspi){
 	HAL_Delay(100);
 }
 
+void Display::write_data(uint8_t* data){
+	HAL_GPIO_WritePin(PORT_INIT, PIN_INIT, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(PORT_CS, PIN_CS, GPIO_PIN_SET);
+	HAL_SPI_Transmit(&hspi,data,1,1000);
+	HAL_GPIO_WritePin(PORT_INIT, PIN_INIT, GPIO_PIN_SET);
+}
+
 // Write a command through SPI
 void Display::write_cmd(uint8_t* cmd){
 	HAL_GPIO_WritePin(PORT_CS, PIN_CS, GPIO_PIN_RESET);
 	HAL_SPI_Transmit(&hspi,cmd,1,1000);
+}
+
+void Display::lcd_print(unsigned char* string){
+	unsigned char * ptrChaine;
+	ptrChaine=string;
+	while(*ptrChaine!=0){
+		write_data(ptrChaine);
+		ptrChaine++;
+	}
+}
+
+void Display::set_cursor(int column,int line){
+	uint8_t val_addr;
+	val_addr = (line * 0x40)+column;
+	val_addr = 0x80+(val_addr & 0x7F);
+	write_cmd(&val_addr);
 }
